07ade085a032251302ba8846c0c7117f68fafffc
alexanderdeyner <deiner.alexander@yandex.ru>
2023-06-01 10:19:30 +0300

    Result 5++

--- src/main/java/spring/mvc/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.mvc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import spring.mvc.models.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
